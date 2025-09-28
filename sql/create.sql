885e59f5d5f634c6a75896d9fd0106d66e6928f2
gasanagerah-ctrl <gasanagerah@gmail.com>
2025-09-28 13:37:07 -0700

    Update create.sql

--- sql/create.sql
+++ sql/create.sql
@@ -1,1 +1,4 @@
-
+customers,products,transactions
+1.create  table customers(customer_id number primary key,name varchar(100),region varchar(100))
+2.create table products(product_id number primary key,name varchar(100),category varchar(100))
+3.create table transactions(transaction_id number primary key,customer_id number references customers(customer_id),product_id number references products(product_id),sale_date date,amount number)
