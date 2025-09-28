1f617522a1872e85a5ad790f67e10ecd476b4d0b
gasanagerah-ctrl <gasanagerah@gmail.com>
2025-09-28 13:43:46 -0700

    Update insert.sql

--- sql/insert.sql
+++ sql/insert.sql
@@ -1,1 +1,7 @@
+inserting in table customers,products,transactions
+  
+1.customers: insert into customers values(1001,'anne marie','kigali')
 
+2.products: insert into products values(2001,'banana coffee','beverages')
+
+3.transactions: insert into transactions values(3001,1001,2001,date '2024-01-15',25000)
