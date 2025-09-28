0386f93941ac1d5c718a3c5d82159d6c93a1945e
gasanagerah-ctrl <gasanagerah@gmail.com>
2025-09-28 13:45:26 -0700

    Update AVG,OVER.sql

--- sql/AVG,OVER.sql
+++ sql/AVG,OVER.sql
@@ -1,1 +1,5 @@
+avg() syntax
 
+SELECT customer_id, sale_date, amount,
+       AVG(amount) OVER (PARTITION BY customer_id ORDER BY sale_date ROWS BETWEEN 1 PRECEDING AND CURRENT ROW) AS moving_avg
+FROM transactions;
