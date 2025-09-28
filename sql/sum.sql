e5d90de2aaf51f6f6914b20ab606aca3b433aed9
gasanagerah-ctrl <gasanagerah@gmail.com>
2025-09-28 13:51:41 -0700

    Update sum.sql

--- sql/sum.sql
+++ sql/sum.sql
@@ -1,1 +1,6 @@
+sum() syntax
+
+SELECT customer_id, sale_date, amount,
+       SUM(amount) OVER (PARTITION BY customer_id ORDER BY sale_date ROWS BETWEEN UNBOUNDED PRECEDING AND CURRENT ROW) AS running_total
+FROM transactions;
 
