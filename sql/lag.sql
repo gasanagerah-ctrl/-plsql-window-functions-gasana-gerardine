8cf604f57811cfcd9fdb48d65b53b43675ab93bd
gasanagerah-ctrl <gasanagerah@gmail.com>
2025-09-28 13:48:13 -0700

    Update lag.sql

--- sql/lag.sql
+++ sql/lag.sql
@@ -1,1 +1,4 @@
-
+LAG() SYNTAX
+SELECT customer_id, transaction_id, sale_date, amount,
+       LAG(amount, 1) OVER (PARTITION BY customer_id ORDER BY sale_date) AS previous_amount
+FROM transactions;
